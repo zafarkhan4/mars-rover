da9dd3ebeae32d301e55fea617ec6c874b2f4ab9
zafarkhan4 <adilzafar3@gmail.com>
2023-02-14 07:59:09 +0000

    Add project setup for Mars Rover

---
+++ src/main/java/com/techreturners/Rover.java
@@ -0,0 +1,5 @@
+package com.techreturners;
+
+public class Rover {
+  
+}
---
+++ src/test/java/com/techreturners/RoverTest.java
@@ -0,0 +1,13 @@
+package com.techreturners;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class RoverTest {
+  
+  @Test
+  public void initialTest() {
+    assertEquals(10+5, 15);
+  }
+}
